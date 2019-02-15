e4a9f5db9db3dd56219a180a0ffee9ed492f5642
Natalia Bustamante <37087760+natalia-bustamante@users.noreply.github.com>
2019-02-14 21:23:30 -0300

    parcial

---
+++ 1-EntradaSalida/dos.js
@@ -0,0 +1,37 @@
+function mostrar()
+{
+    /*var precio;
+	var descuento;
+	var iva;
+	var porcentaje;
+
+
+	precio=valor1.value;
+	descuento=valor2.value;
+	iva=valor3.value;
+
+	precio=parseInt(precio);
+	descuento=parseInt(descuento);
+	iva=parseInt(iva);
+
+	porcentaje=precio%descuento;
+	suma=porcentaje+iva;*/
+
+	var compra;
+	var descuento;
+	var porcentaje;
+
+	compra=valor1.value;
+	descuento=valor2.value;
+
+	compra=parseInt(compra);
+	descuento=parseInt(descuento);
+
+	porcentaje=compra%descuento;
+
+	alert("tu descuento es" + "porcentaje");
+
+	/*alert (tu compra es de xx tenes un descuento del
+	 xx queda en xx, mas el iva es )*/
+  
+}
---
+++ 1-EntradaSalida/uno.js
@@ -0,0 +1,30 @@
+
+function mostrar()
+{
+    var mascota1;
+	var mascota2;
+	var suma;
+
+	mascota1=peso1.value;
+	mascota2=peso2.value;
+
+	mascota1=parseInt(mascota1);
+	mascota2=parseInt(mascota2);
+
+	suma=mascota1+mascota2;
+
+	alert("jgfjg");
+
+	/*alert("Tenes dos mascotas" +mascota1+ "y" 
+		+mascota2+ "que pesan en total: " + suma);*/
+
+		/*var mascota1;
+		var mascota2;
+		var suma;
+
+		mascota1=prompt.(Ingrese nombre);
+
+		alert("Tenes dos mascotas: "+ suma);*/
+}
+
+
