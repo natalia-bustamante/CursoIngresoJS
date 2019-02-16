26421531ce97c1353d2b6aa694a937d1b981d9de
Natalia Bustamante <37087760+natalia-bustamante@users.noreply.github.com>
2019-02-15 22:04:33 -0300

    if
    9 10

---
+++ jsInstruccionIF (10).js
@@ -0,0 +1,34 @@
+function mostrar()
+{
+	var nota;
+	var mensaje;
+
+	nota=Math.floor((Math.random()*10+1);
+	if (nota)
+
+	//console.log(numeroRandom);//
+ 	if(nota>8)
+ 	{
+ 		mensaje="Excelente su nota es: "+nota;
+ 	}
+ 	
+ 	else
+ 	{	
+ 				if(nota>3)
+ 				{   
+ 	
+ 				mensaje="Aprobó, su nota es: "+nota;
+
+ 				}
+ 				else(nota<4)
+ 				{
+ 				mensaje="Vamos, la próxima se puede" +nota;
+ 				}
+
+ 	 }
+ 	 alert(mensaje);
+
+	//Genero el número RANDOM entre 1 y 10 
+	
+
+}//FIN DE LA FUNCIÓN
---
+++ jsInstruccionIF (9).js
@@ -0,0 +1,12 @@
+function mostrar()
+{
+	var numeroRandom;
+	numeroRandom=Math.floor((Math.random()*10+1);
+	console.log(numeroRandom);
+
+	
+
+	//Genero el número RANDOM entre 1 y 10 
+	
+
+}//FIN DE LA FUNCIÓN
