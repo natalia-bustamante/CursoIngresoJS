ff4f2703801ba08828758eb0b476cc1da1e97680
Natalia Bustamante <37087760+natalia-bustamante@users.noreply.github.com>
2019-02-21 21:19:09 -0300

    rec

---
+++ cinco.js
@@ -0,0 +1,38 @@
+function mostrar()
+{
+	var precio;
+	var visa;
+	var mercadoPago;
+	var efectivo;
+	var paypal;
+	var otro;
+	var todoIncluido;
+	var soloDesayunos;
+	var mensaje;
+
+	precio=prompt('Ingrese el precio');
+
+
+	precio=getElementById('precio').value;
+
+	precio*10/100;
+
+
+	switch (precio)
+	{
+		case"todoIncluido+paypal":
+		mensaje="";
+		break;
+		case"soloDesayunos+efectivo":
+		mensaje="";
+		break;
+		case"todoIncluido+efectivo;":
+		break;
+
+	}
+
+
+
+
+
+}
---
+++ cuatro.js
@@ -0,0 +1,46 @@
+function mostrar()
+{
+	var primerNumero;
+	var segundoNumero;
+	var tercerNumero;
+	var cuartoNumero;
+	var suma;
+	var mensaje;
+	var resultadoFinal;
+
+
+	primerNumero=getElementById('primerNumero').value;
+	division=getElementById('division').value;
+	suma=getElementById('suma').value;
+	resultado=getElementById('resultado').value;
+
+	/*primerNumero.parseInt*/
+
+	primerNumero=prompt('Ingresar primer Numero');
+
+
+
+	if (primerNumero==segundoNumero);
+	{ 
+	
+		mensaje="";
+	}
+
+	  else		
+		{ /*(primerNumero>50);	
+ 
+			division=primerNumero/segundoNumero;
+			mensaje="";*/
+
+			if(suma<50);
+			{
+				resultado;
+			}
+
+		}
+		
+
+
+	alert(mensaje);
+
+}
