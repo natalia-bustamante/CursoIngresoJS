acc708eec13cd057ab02173f2078dafaa3342858
Natalia Bustamante <37087760+natalia-bustamante@users.noreply.github.com>
2019-02-21 22:11:46 -0300

    Add files via upload

---
+++ jsIteraciones (5).js
@@ -0,0 +1,19 @@
+function mostrar()
+{
+
+var sexo;
+
+sexo = prompt("ingrese f ó m .");
+
+while(sexo f )
+{
+	sexo = prompt("Femenino")
+	sexo = prompt("Masculino")
+
+}
+
+document.getElementById('Sexo').value=sexo;
+
+alert(sexo);
+
+}//FIN DE LA FUNCIÓN
