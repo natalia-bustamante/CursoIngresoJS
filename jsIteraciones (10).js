181dd6954c85685fdfed113c63aa8f5453d30fd1
Natalia Bustamante <37087760+natalia-bustamante@users.noreply.github.com>
2019-02-22 22:03:32 -0300

    Add files via upload

---
+++ jsIteraciones (10).js
@@ -0,0 +1,60 @@
+function mostrar()
+{
+
+	var contador=0;
+	var negativos;
+	var positivos;
+	var numero;
+	var ceros;
+	var pares;
+	var contadorDePares;
+	var contadorNegativos;
+	var contadorPositivos;
+	var acumuladorNegativos;
+	var acumuladorPositivos;
+	var bandera;
+	var diferencia;
+	
+
+	//declarar contadores y variables 
+	var respuesta="si";
+
+	contadorDePares=0;
+	contadorPositivos=0,
+	ceros=0;
+	contadorNegativos=0;
+	acumuladorPositivos=0;
+	acumuladorNegativos=0;
+
+
+	//suma=positivos+positivos;
+	//suma=negativos+negativos;
+	
+	while(contador<5)
+	{
+		contador=contador+1;
+		numero=prompt("Ingrese numero"+contador+"#:");
+		numero=parseInt(numero);
+		respuesta=prompt("=> si para continuar")
+		if(numero>0)
+		{
+			//positivos=positivos+numero;
+			acumuladorNegativos+acumuladorNegativos;	
+		}else
+		{
+			if (true) 
+			{
+				acumuladorPositivos+acumuladorPositivos;
+
+			}
+		}
+
+		
+	}
+	promediopositivos=acumuladorPositivos/contadorPositivos;
+	promenegativos=acumuladorNegativos/contadorNegativos;
+	DIFERENCIA
+
+
+}//FIN DE LA FUNCIÓNcontador=contador+1;
+		
---
+++ siete.js
@@ -0,0 +1,27 @@
+function mostrar()
+{
+	var notas;
+	var sexo;
+	var contador=0;
+	var promedio;
+	var mensaje;
+	//var acumuladorDeNotas;
+	
+
+
+	while(contador<5)
+	{
+		contador=contador+1;
+		notas=prompt("Ingresar la nota");
+		sexo=prompt("Ingresar sexo: 'f' o 'm'");
+		notas=parseInt(notas);
+
+	}
+	/*if (notas>0) 
+	{
+		acumuladorDeNotas+acumuladorDeNotas=promedio;
+	}*/
+
+	alert(mensaje);
+
+}
